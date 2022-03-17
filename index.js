2c15493ff165163caecfdd6591c3acc180af704a
William Luck <luck.william.h@gmail.com>
2022-03-17 11:53:43 -0500

    commit tree 1286e6d4c9a732d28fcf98ff28c5e0f98327aa61

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-// Code your solutions in this file
+// let squaf = [
+//     "Biblo",
+//     "Tot",
+//     'diegs',
+//     'bert',
+//     'ali'
+//   ]
+
+const names = ["Guadalupe", "Ollie", "Aki"]
+
+function writeCards(arr, eventName) {
+    const newArr = []  
+    for (let member of arr) { 
+        newArr.push(`Thank you, ${member}, for the wonderful ${eventName} gift!`);
+      }
+    return newArr;
+  }
+
+  // while loop to count down to zero, provided an integer. 
+
+function countDown(startingNum) {
+    while (startingNum > 0) {
+        console.log(startingNum);
+        startingNum--;
+    }
+    console.log(startingNum);
+}
+
+countDown(10);
+
+
+
+
+
+
+
+
+
